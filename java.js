688b3f6468d94bae15cece95cf467c43ab2efabb
BlopOG <Ian.Gregory.Giles@gmail.com>
2026-07-28 21:33:47 -0400

    part two

--- java.js
+++ java.js
@@ -26,3 +26,23 @@ for (const item of adventurer.inventory) {
   console.log(`- ${item}`);
 }
 adventurer.roll();
+
+// Part 2: Class Fantasy
+console.log(" PART 2 & PART 4: CHARACTER BASE CLASS ");
+
+class Character {
+  
+  static MAX_HEALTH = 100;
+
+  constructor(name) {
+    this.name = name;
+    this.health = Character.MAX_HEALTH;
+    this.inventory = [];
+  }
+
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+    return result;
+  }
+}
